fcbb67a92aff5f8fa27692fcfcbdb67100dbb8fd
Aaron Solomon Adelman <Aaron.Solomon.Adelman@gmail.com>
2020-04-05 19:07:14 +0300

    More efficiency

--- Date-O-Rama/Views/ASAMainRowsView.swift
+++ Date-O-Rama/Views/ASAMainRowsView.swift
@@ -10,20 +10,34 @@ import SwiftUI
 
 
 struct ASAMainRowsView: View {
-    @State var mainRows:  Array<ASARow> = [ASARow.test()]
+    @State var rows:  Array<ASARow> = [ASARow.test()]
 
     var body: some View {
         NavigationView {
-            MasterView(rows: self.$mainRows)
+//            MasterView(rows: self.$rows)
+            List {
+                ForEach(rows, id:  \.uid) { row in
+                    NavigationLink(
+                        destination: DetailView(selectedRow: row )
+                    ) {
+                        Text(row.dateString(now: Date()))
+                    }
+                }
+                .onDelete { indices in
+                    indices.forEach {
+                        debugPrint("\(#file) \(#function)")
+                        self.rows.remove(at: $0) }
+                }
+            }
                 .navigationBarTitle(Text("Date-O-Rama"))
                 .navigationBarItems(
                     leading: EditButton(),
                     trailing: Button(
                         action: {
                             withAnimation {
-                                debugPrint("\(#file) \(#function) + button, \(self.mainRows.count) rows before")
-                                self.mainRows.insert(ASARow.generic(), at: 0)
-                                debugPrint("\(#file) \(#function) + button, \(self.mainRows.count) rows after")
+                                debugPrint("\(#file) \(#function) + button, \(self.rows.count) rows before")
+                                self.rows.insert(ASARow.generic(), at: 0)
+                                debugPrint("\(#file) \(#function) + button, \(self.rows.count) rows after")
                             }
                         }
                     ) {
@@ -35,25 +49,26 @@ struct ASAMainRowsView: View {
     }
 }
 
-struct MasterView: View {
-    @Binding var rows:  Array<ASARow>
-    
-    var body: some View {
-        List {
-            ForEach(rows, id:  \.uid) { row in
-                NavigationLink(
-                    destination: DetailView(selectedRow: row )
-                ) {
-                    Text(row.dateString(now: Date()))
-                }
-            }.onDelete { indices in
-                indices.forEach {
-                    debugPrint("\(#file) \(#function)")
-                    self.rows.remove(at: $0) }
-            }
-        }
-    }
-}
+//struct MasterView: View {
+//    @Binding var rows:  Array<ASARow>
+//
+//    var body: some View {
+//        List {
+//            ForEach(rows, id:  \.uid) { row in
+//                NavigationLink(
+//                    destination: DetailView(selectedRow: row )
+//                ) {
+//                    Text(row.dateString(now: Date()))
+//                }
+//            }
+//            .onDelete { indices in
+//                indices.forEach {
+//                    debugPrint("\(#file) \(#function)")
+//                    self.rows.remove(at: $0) }
+//            }
+//        }
+//    }
+//}
 
 
 struct ASADetailCell:  View {
@@ -68,9 +83,29 @@ struct ASADetailCell:  View {
     }
 }
 
-struct DetailView: View {
-    var selectedRow:  ASARow?
+struct ASADetail {
+    var name:  String
+    var geekCode:  String
+}
 
+struct DetailView: View {
+    let details:  Array<ASADetail> = [
+        ASADetail(name: "Era", geekCode: "GGGG"),
+        ASADetail(name: "Calendar year", geekCode: "y"),
+        ASADetail(name: "Month", geekCode: "MMMM"),
+        ASADetail(name: "Day of month", geekCode: "d"),
+        ASADetail(name: "Weekday", geekCode: "eeee"),
+        ASADetail(name: "Quarter", geekCode: "QQQQ"),
+        ASADetail(name: "Year (for “week of year”)", geekCode: "Y"),
+        ASADetail(name: "Week of year", geekCode: "w"),
+        ASADetail(name: "Week of month", geekCode: "W"),
+        ASADetail(name: "Day of week in month", geekCode: "F"),
+        ASADetail(name: "Day of year", geekCode: "D"),
+        ASADetail(name: "Cyclic year name", geekCode: "UUUU"),
+        ASADetail(name: "Related Gregorian year", geekCode: "r"),
+        ASADetail(name: "Modified Julian day", geekCode: "g")    ]
+    @State var selectedRow:  ASARow?
+    
     var body: some View {
         List {
             if selectedRow != nil {
@@ -83,30 +118,14 @@ struct DetailView: View {
                     }
                 }
                 Section(header:  Text("Date")) {
-                    Group {
-                        ASADetailCell(title: "Era", detail: selectedRow!.dateString(now: Date(), LDMLString: "GGGG"))
-                        
-                        ASADetailCell(title: "Calendar year", detail: selectedRow!.dateString(now: Date(), LDMLString: "y"))
-                        ASADetailCell(title: "Month", detail: selectedRow!.dateString(now: Date(), LDMLString: "MMMM"))
-                        ASADetailCell(title: "Day of month", detail: selectedRow!.dateString(now: Date(), LDMLString: "d"))
-                        ASADetailCell(title: "Weekday", detail: selectedRow!.dateString(now: Date(), LDMLString: "eeee"))
-                    }
-                    Group {
-                        ASADetailCell(title: "Quarter", detail: selectedRow!.dateString(now: Date(), LDMLString: "QQQQ"))
-                        
-                        ASADetailCell(title: "Year (for “week of year”)", detail: selectedRow!.dateString(now: Date(), LDMLString: "Y"))
-                        ASADetailCell(title: "Week of year", detail: selectedRow!.dateString(now: Date(), LDMLString: "w"))
-                        
-                        ASADetailCell(title: "Week of month", detail: selectedRow!.dateString(now: Date(), LDMLString: "W"))
-                        ASADetailCell(title: "Day of week in month", detail: selectedRow!.dateString(now: Date(), LDMLString: "F"))
-                        
-                        ASADetailCell(title: "Day of year", detail: selectedRow!.dateString(now: Date(), LDMLString: "D"))
-                        
-                        ASADetailCell(title: "Cyclic year name", detail: selectedRow!.dateString(now: Date(), LDMLString: "UUUU"))
-                        
-                        ASADetailCell(title: "Related Gregorian year", detail: selectedRow!.dateString(now: Date(), LDMLString: "r"))
-                        
-                        ASADetailCell(title: "Modified Julian day", detail: selectedRow!.dateString(now: Date(), LDMLString: "g"))
+                    ForEach(self.details, id: \.name) {
+                        detail
+                        in
+                        HStack {
+                            Text(verbatim:  detail.name).bold()
+                            Spacer()
+                            Text(verbatim:  (self.selectedRow?.dateString(now: Date(), LDMLString: detail.geekCode)) ?? "")
+                        }
                     }
                 }
             } else {
