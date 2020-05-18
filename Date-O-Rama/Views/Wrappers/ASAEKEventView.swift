e7873a4badaa7e2317b4df20dbdab3a73676de9d
Aaron Solomon Adelman <Aaron.Solomon.Adelman@gmail.com>
2020-05-18 17:31:15 +0300

    It works, but it's buggy.

--- Date-O-Rama/Views/Wrappers/ASAEKEventView.swift
+++ Date-O-Rama/Views/Wrappers/ASAEKEventView.swift
@@ -12,15 +12,41 @@ import EventKit
 import EventKitUI
 
 struct ASAEKEventView: UIViewControllerRepresentable {
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+    
     var event:  EKEvent
     
     func makeUIViewController(context: Context) -> EKEventViewController {
         let eventViewController = EKEventViewController()
         eventViewController.event = event
+        eventViewController.delegate = context.coordinator
         return eventViewController
     } // func makeUIViewController(context: Context) -> EKEventViewController
     
     func updateUIViewController(_ uiViewController: EKEventViewController, context: Context) {
         
     } // func updateUIViewController(_ uiViewController: EKEventViewController, context: Context)
+    
+    class Coordinator:  NSObject, EKEventViewDelegate {
+        func eventViewController(_ controller: EKEventViewController, didCompleteWith action: EKEventViewAction) {
+            switch action {
+            case .done:
+                debugPrint(#file, #function, "Done")
+                
+            case .responded:
+                debugPrint(#file, #function, "Responded")
+                
+            case .deleted:
+                debugPrint(#file, #function, "Deleted")
+                
+            @unknown default:
+                debugPrint(#file, #function, "Unknown default")
+                
+            }
+        }
+        
+        
+    }
 } // struct ASAEKEventView
