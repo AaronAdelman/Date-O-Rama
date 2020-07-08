46da37ea7cb5f13dfbcbc3f278cc1b3b1f15b2f2
Aaron Solomon Adelman <Aaron.Solomon.Adelman@gmail.com>
2020-07-08 15:19:16 +0300

    Cleanup.

--- Date-O-Rama/Events/ASASolarEventSource.swift
+++ Date-O-Rama/Events/ASASolarEventSource.swift
@@ -11,7 +11,7 @@ import SwiftUI
 import CoreLocation
 
 class ASASolarEventSource:  ASAInternalEventSource {
-    var JSONData:  ASAEventsFile
+    var eventsFile:  ASAEventsFile
 
     init() {
         let fileURL = Bundle.main.url(forResource:"Solar events", withExtension: "json")!
@@ -19,13 +19,12 @@ class ASASolarEventSource:  ASAInternalEventSource {
         let jsonData = (try? Data(contentsOf: fileURL))!
         let newJSONDecoder = JSONDecoder()
         let eventsFile = try? newJSONDecoder.decode(ASAEventsFile.self, from: jsonData)
-        self.JSONData = eventsFile!
+        self.eventsFile = eventsFile!
     }
     
-//    var eventSourceCode: ASAInternalEventSourceCode = .solar
     var eventSourceCode: ASAInternalEventSourceCode {
         get {
-            return ASAInternalEventSourceCode.init(rawValue: self.JSONData.eventSourceCode)!
+            return ASAInternalEventSourceCode.init(rawValue: self.eventsFile.eventSourceCode)!
         }
     }
     
@@ -51,64 +50,34 @@ class ASASolarEventSource:  ASAInternalEventSource {
     } // func eventDetails(startDate: Date, endDate: Date, location: CLLocation, timeZone: TimeZone, eventCalendarName: String) -> Array<ASAEvent>
     
     fileprivate func calendarColor() -> Color {
-//        return Color(UIColor.yellow)
-        return Color(self.JSONData.calendarColor)
+        return Color(self.eventsFile.calendarColor)
     } // static func calendarColor() -> Color
     
     func eventDetails(date:  Date, location:  CLLocation, timeZone:  TimeZone, eventCalendarName: String) -> Array<ASAEvent> {
         let latitude  = location.coordinate.latitude
         let longitude = location.coordinate.longitude
         
-//        let events = date.solarEvents(latitude: latitude, longitude: longitude, events: [.sunrise, .sunset, .civilDawn, .civilDusk, .nauticalDawn, .nauticalDusk, .astronomicalDawn, .astronomicalDusk], timeZone:  timeZone)
-//
-//        let sunrise:  Date = events[.sunrise]!! // נץ
-//        let sunset:  Date = events[.sunset]!! // שקיעה
-//
-//        let civilDawn:  Date = events[.civilDawn]!!
-//        let civilDusk:  Date = events[.civilDusk]!!
-//
-//        let nauticalDawn:  Date = events[.nauticalDawn]!!
-//        let nauticalDusk:  Date = events[.nauticalDusk]!!
-//
-//        let astronomicalDawn:  Date = events[.astronomicalDawn]!!
-//        let astronomicalDusk:  Date = events[.astronomicalDusk]!!
-//
-//        return [
-//            ASAEvent(title: NSLocalizedString(ASTRONOMICAL_DAWN_KEY, comment: ""), startDate: astronomicalDawn, endDate: astronomicalDawn, isAllDay: false, timeZone: timeZone, color: calendarColor(), calendarTitle:  eventCalendarName),
-//            ASAEvent(title: NSLocalizedString(NAUTICAL_DAWN_KEY, comment: ""), startDate: nauticalDawn, endDate: nauticalDawn, isAllDay: false, timeZone: timeZone, color: calendarColor(), calendarTitle:  eventCalendarName),
-//            ASAEvent(title: NSLocalizedString(CIVIL_DAWN_KEY, comment: ""), startDate: civilDawn, endDate: civilDawn, isAllDay: false, timeZone: timeZone, color: calendarColor(), calendarTitle:  eventCalendarName),
-//
-//            ASAEvent(title: NSLocalizedString(SUNRISE_KEY, comment: ""), startDate: sunrise, endDate: sunrise, isAllDay: false, timeZone: timeZone, color: calendarColor(), calendarTitle:  eventCalendarName),
-//
-//            ASAEvent(title: NSLocalizedString(SUNSET_KEY, comment: ""), startDate: sunset, endDate: sunset, isAllDay: false, timeZone: timeZone, color: calendarColor(), calendarTitle:  eventCalendarName),
-//
-//            ASAEvent(title: NSLocalizedString(CIVIL_DUSK_KEY, comment: ""), startDate: civilDusk, endDate: civilDusk, isAllDay: false, timeZone: timeZone, color: calendarColor(), calendarTitle:  eventCalendarName),
-//            ASAEvent(title: NSLocalizedString(NAUTICAL_DUSK_KEY, comment: ""), startDate: nauticalDusk, endDate: nauticalDusk, isAllDay: false, timeZone: timeZone, color: calendarColor(), calendarTitle:  eventCalendarName),
-//            ASAEvent(title: NSLocalizedString(ASTRONOMICAL_DUSK_KEY, comment: ""), startDate: astronomicalDusk, endDate: astronomicalDusk, isAllDay: false, timeZone: timeZone, color: calendarColor(), calendarTitle:  eventCalendarName)
-//        ]
         var result:  Array<ASAEvent> = []
-        for codableEvent in self.JSONData.events {
-            let title = NSLocalizedString(codableEvent.localizableTitle!, comment: "")
-            if codableEvent.type == .degreesBelowHorizon {
-                let solarEvent = ASASolarEvent(degreesBelowHorizon: codableEvent.degreesBelowHorizon!, rising: codableEvent.rising!, offset: codableEvent.offset!)
+        for programmedEvent in self.eventsFile.events {
+            let title = NSLocalizedString(programmedEvent.localizableTitle!, comment: "")
+            if programmedEvent.type == .degreesBelowHorizon {
+                let solarEvent = ASASolarEvent(degreesBelowHorizon: programmedEvent.degreesBelowHorizon!, rising: programmedEvent.rising!, offset: programmedEvent.offset!)
                 
                 let events = date.solarEvents(latitude: latitude, longitude: longitude, events: [solarEvent], timeZone:  timeZone)
                 let date = events[solarEvent]
                 
-                let newEvent = ASAEvent(title: title, startDate: date!!, endDate: date!!, isAllDay: false, timeZone: timeZone, color: Color(self.JSONData.calendarColor), calendarTitle: eventCalendarName)
+                let newEvent = ASAEvent(title: title, startDate: date!!, endDate: date!!, isAllDay: false, timeZone: timeZone, color: Color(self.eventsFile.calendarColor), calendarTitle: eventCalendarName)
                 result.append(newEvent)
             }
-        } // for codableEvent in self.JSONData.events
+        } // for programmedEvent in self.eventsFile.events
         return result
     } // func eventDetails(date:  Date, location:  CLLocation, timeZone:  TimeZone, eventCalendarName: String) -> Array<ASAEvent>
     
     func eventCalendarName(locationData:  ASALocationData) -> String {
-//        return "\(NSLocalizedString("Solar events", comment: "")) • \(locationData.formattedOneLineAddress())"
-        return "\(NSLocalizedString(self.JSONData.localizableTitle!, comment: "")) • \(locationData.formattedOneLineAddress())"
+        return "\(NSLocalizedString(self.eventsFile.localizableTitle!, comment: "")) • \(locationData.formattedOneLineAddress())"
     } // func eventCalendarName(locationData:  ASALocationData) -> String
     
     func eventSourceName() -> String {
-//        return NSLocalizedString("Daily solar events", comment:  "")
-        return NSLocalizedString(self.JSONData.localizableTitle!, comment:  "")
+        return NSLocalizedString(self.eventsFile.localizableTitle!, comment:  "")
     } // func eventSourceName() -> String} // class ASASolarEventSource:  ASAInternalEventSource
 }
