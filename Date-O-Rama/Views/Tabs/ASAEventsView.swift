230b8cb71227c555387be9d37ecbfe3e7a34df8a
Aaron Solomon Adelman <Aaron.Solomon.Adelman@gmail.com>
2020-09-22 16:45:50 +0300

    Twiddling with the date picker.

--- Date-O-Rama/Views/Tabs/ASAEventsView.swift
+++ Date-O-Rama/Views/Tabs/ASAEventsView.swift
@@ -96,7 +96,9 @@ struct ASAEventsView: View {
     var body: some View {
         NavigationView {
             VStack {
-                ASADatePicker(date: $date, primaryRow: self.primaryRow, showingDatePicker: false)
+                ASADatePicker(date: $date, primaryRow: self.primaryRow
+//                              , showingDatePicker: false
+                )
 
                 List {
                     NavigationLink(destination:  ASARowChooser(selectedUUIDString:  $settings.primaryRowUUIDString)) {
@@ -203,7 +205,7 @@ struct ASADatePicker:  View {
 
         } // didSet
     } // var primaryRow
-    @State var showingDatePicker:  Bool
+//    @State var showingDatePicker:  Bool
     
     #if os(iOS)
     #if targetEnvironment(macCatalyst)
@@ -219,22 +221,22 @@ struct ASADatePicker:  View {
     
     var body: some View {
         HStack {
-            if runningOnIOS {
-                Toggle(isOn: self.$showingDatePicker) {
-                    Text("")
-                }
-                .frame(width:  SPECIAL_SPACER_WIDTH)
-            }
+//            if runningOnIOS {
+//                Toggle(isOn: self.$showingDatePicker) {
+//                    Text("")
+//                }
+//                .frame(width:  SPECIAL_SPACER_WIDTH)
+//            }
             
             Spacer()
             
-            if !self.showingDatePicker || !runningOnIOS {
+//            if !self.showingDatePicker || !runningOnIOS {
                 Button(action: {
                     self.date = self.date.oneDayBefore
                 }) {
                     Text("🔺").font(BOTTOM_BUTTONS_FONT_SIZE)
                 }
-            }
+//            }
             
             
             Button(action: {
@@ -243,26 +245,26 @@ struct ASADatePicker:  View {
                 Text("Today").font(BOTTOM_BUTTONS_FONT_SIZE)
             }.foregroundColor(.accentColor)
             
-            if !self.showingDatePicker || !runningOnIOS {
+//            if !self.showingDatePicker || !runningOnIOS {
                 Button(action: {
                     self.date = self.date.oneDayAfter
                 }) {
                     Text("🔻").font(BOTTOM_BUTTONS_FONT_SIZE)
                 }
-            }
+//            }
             
             Spacer()
 
-            if self.showingDatePicker || !runningOnIOS {
+//            if self.showingDatePicker || !runningOnIOS {
                 DatePicker(selection:  self.$date, in:  Date.distantPast...Date.distantFuture, displayedComponents: .date) {
                     Text("")
                 }
                 Spacer()
-            }
+//            }
             
-            if !self.showingDatePicker && runningOnIOS {
-                Spacer().frame(width:  SPECIAL_SPACER_WIDTH)
-            }
+//            if !self.showingDatePicker && runningOnIOS {
+//                Spacer().frame(width:  SPECIAL_SPACER_WIDTH)
+//            }
 
         }
         .border(Color.gray)
