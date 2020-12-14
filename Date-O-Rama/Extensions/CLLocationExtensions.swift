c3d79b19ece0f29b90d6322ef22d49f89062b393
Aaron Solomon Adelman <Aaron.Solomon.Adelman@gmail.com>
2020-12-14 19:07:50 +0200

    Made coordinate presentation snazzier.

--- Date-O-Rama/Extensions/CLLocationExtensions.swift
+++ Date-O-Rama/Extensions/CLLocationExtensions.swift
@@ -9,16 +9,34 @@
 import Foundation
 import CoreLocation
 
+// From https://stackoverflow.com/questions/27996351/swift-convert-decimal-coordinate-into-degrees-minutes-seconds-direction
+extension BinaryFloatingPoint {
+    var dms: (degrees: Int, minutes: Int, seconds: Int) {
+        var seconds = Int(self * 3600)
+        let degrees = seconds / 3600
+        seconds = abs(seconds % 3600)
+        return (degrees, seconds / 60, seconds % 60)
+    }
+
+    var dmsString:  String {
+        let (d, m, s) = self.dms
+        return "\(d)°\(m)′\(s)″"
+    }
+}
+
+
+// MARK:  -
+
 extension CLLocation {
     var humanInterfaceRepresentation:  String {
         get {
             let absoluteLatitude:  Double = abs(self.coordinate.latitude)
-            let absoluteLatitudeString = String(format: "%.4f", absoluteLatitude)
+            let absoluteLatitudeString = absoluteLatitude.dmsString
             let latituteDirection = self.coordinate.latitude >= 0.0 ? "N" : "S"
             let absoluteLongitude:  Double = abs(self.coordinate.longitude)
-            let absoluteLongitudeString = String(format: "%.4f", absoluteLongitude)
+            let absoluteLongitudeString = absoluteLongitude.dmsString
             let longitudeDirection = self.coordinate.longitude >= 0.0 ? "E" : "W"
-            var result = "\(absoluteLatitudeString)°\(latituteDirection) \(absoluteLongitudeString)°\(longitudeDirection)"
+            var result = "\(absoluteLatitudeString)\(latituteDirection) \(absoluteLongitudeString)\(longitudeDirection)"
 
             if self.altitude != 0.0 {
                 let altitudeString = String(format: "%.0f", self.altitude)
