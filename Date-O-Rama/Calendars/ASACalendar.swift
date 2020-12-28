a4f82a60234b3a091dddb2e0527e77df8bfe95c1
Aaron Solomon Adelman <Aaron.Solomon.Adelman@gmail.com>
2020-12-28 13:27:05 +0200

    Cleanup.

--- Date-O-Rama/Calendars/ASACalendar.swift
+++ Date-O-Rama/Calendars/ASACalendar.swift
@@ -10,11 +10,6 @@ import Foundation
 import CoreLocation
 import UIKit
 
-//struct ASALDMLDetail {
-//    var name:  String
-//    var geekCode:  String
-//} // struct ASADetail
-
 public enum ASATransitionType {
     case sunset
     case dusk
@@ -30,7 +25,6 @@ protocol ASACalendar {
     var canSplitTimeFromDate:  Bool { get }
     var defaultDateFormat:  ASADateFormat { get }
     var defaultTimeFormat:  ASATimeFormat { get }
-    //    var LDMLDetails: Array<ASALDMLDetail> { get }
     var supportedDateFormats: Array<ASADateFormat> { get }
     var supportedWatchDateFormats: Array<ASADateFormat> { get }
     var supportedTimeFormats: Array<ASATimeFormat> { get }
@@ -42,14 +36,7 @@ protocol ASACalendar {
     var supportsTimeZones: Bool { get }
     var transitionType:  ASATransitionType { get }
     
-    //    func defaultDateGeekCode(dateFormat:  ASADateFormat) -> String
-    //    func defaultTimeGeekCode(timeFormat:  ASATimeFormat) -> String
-    func dateTimeString(now:  Date, localeIdentifier:  String, dateFormat:  ASADateFormat,
-                        //                        dateGeekFormat: String,
-                        timeFormat: ASATimeFormat,
-                        //                        timeGeekFormat: String,
-                        locationData:  ASALocationData) -> String
-    //    func dateTimeString(now:  Date, localeIdentifier:  String, LDMLString:  String, locationData:  ASALocationData) -> String
+    func dateTimeString(now:  Date, localeIdentifier:  String, dateFormat:  ASADateFormat, timeFormat: ASATimeFormat, locationData:  ASALocationData) -> String
     func startOfDay(for date:  Date, location:  CLLocation, timeZone:  TimeZone) -> Date
     func startOfNextDay(date:  Date, location:  CLLocation, timeZone:  TimeZone) -> Date
     
@@ -71,8 +58,7 @@ protocol ASACalendar {
     func minimumRange(of component: ASACalendarComponent) -> Range<Int>? // Returns the minimum range limits of the values that a given component can take on.
     func ordinality(of smaller: ASACalendarComponent, in larger: ASACalendarComponent, for date: Date) -> Int? // Returns, for a given absolute time, the ordinal number of a smaller calendar component (such as a day) within a specified larger calendar component (such as a week).
     func range(of smaller: ASACalendarComponent, in larger: ASACalendarComponent, for date: Date) -> Range<Int>? // Returns the range of absolute time values that a smaller calendar component (such as a day) can take on in a larger calendar component (such as a month) that includes a specified absolute time.
-    
-    //    func containingComponent(of component:  ASACalendarComponent) -> ASACalendarComponent? // Returns which component contains the specified component for specifying a date.  E.g., in many calendars days are contained within months, months are contained within years, and years are contained within eras.
 
+    // MARK:  - Symbols
     func veryShortStandaloneWeekdaySymbols(localeIdentifier:  String) -> Array<String>
 } // protocol ASACalendar
