fae6adf674cc7c98dae1ec943248c11c92ddfcfa
Aaron Solomon Adelman <Aaron.Solomon.Adelman@gmail.com>
2021-01-20 21:49:02 +0200

    Proof of concept for using PDFs for FAQs.

--- Date-O-Rama/Views/Tabs/ASAAboutView.swift
+++ Date-O-Rama/Views/Tabs/ASAAboutView.swift
@@ -10,26 +10,34 @@ import SwiftUI
 
 struct ASAAboutView: View {
     var body: some View {
-        VStack {
-            let SCALE = 2.0
-            Image(uiImage: UIImage(named: "AppIcon") ?? UIImage())
-                .cornerRadius(2.0)
-                .scaleEffect(CGSize(width: SCALE, height: SCALE))
+        NavigationView {
+            VStack {
+                let SCALE = 2.0
+                Image(uiImage: UIImage(named: "AppIcon") ?? UIImage())
+                    .cornerRadius(2.0)
+                    .scaleEffect(CGSize(width: SCALE, height: SCALE))
 
-            Spacer()
-                .frame(height:  8.0)
+                Spacer()
+                    .frame(height:  8.0)
 
-            let mainBundle: Bundle = Bundle.main
-            let appName = (mainBundle.infoDictionary?["CFBundleName"] as? String) ?? ""
-            let version: String = mainBundle.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? ""
-            let build:  String = mainBundle.infoDictionary?["CFBundleVersion"] as? String ?? ""
-            Text("\(appName) \(version) (\(build))")
-                .font(.largeTitle)
+                let mainBundle: Bundle = Bundle.main
+                let appName = (mainBundle.infoDictionary?["CFBundleName"] as? String) ?? ""
+                let version: String = mainBundle.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? ""
+                let build:  String = mainBundle.infoDictionary?["CFBundleVersion"] as? String ?? ""
+                Text("\(appName) \(version) (\(build))")
+                    .font(.largeTitle)
 
-            Text("by Aaron Solomon Adelman")
-            Link("✉️ Aaron.Solomon.Adelman@gmail.com", destination:  URL(string: "mailto:Aaron.Solomon.Adelman@gmail.com?subject=Date-O-Rama")!)
-        } // VStack
+                Text("by Aaron Solomon Adelman")
+                Link("✉️ Aaron.Solomon.Adelman@gmail.com", destination:  URL(string: "mailto:Aaron.Solomon.Adelman@gmail.com?subject=Date-O-Rama")!)
 
+                List {
+                    NavigationLink(destination:                     ASALocalPDFView(fileName: "Thanks to")) {
+                        Text("Thanks to…")
+                    }
+                }
+            } // VStack
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
     } // var body
 } // struct ASAAboutView
 
