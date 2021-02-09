89707624f46ebe1333098e04b08ba1ab255f4b8c
Aaron Solomon Adelman <Aaron.Solomon.Adelman@gmail.com>
2021-02-09 13:03:16 +0200

    Removed unused code.

--- Date-O-Rama/Extensions/ArrayExtensions.swift
+++ Date-O-Rama/Extensions/ArrayExtensions.swift
@@ -9,42 +9,42 @@
 import EventKit
 import Foundation
 
-extension Array where Element == Int {
-    var EKWeekdays:  Array<EKWeekday> {
-        var result: Array<EKWeekday> = []
-        for weekday in self {
-            assert(weekday >= 1)
-            assert(weekday <= 7)
-
-            switch weekday {
-            case 1:
-                result.append(.sunday)
-
-            case 2:
-                result.append(.monday)
-
-            case 3:
-                result.append(.tuesday)
-
-            case 4:
-                result.append(.wednesday)
-
-            case 5:
-                result.append(.thursday)
-
-            case 6:
-                result.append(.friday)
-
-            case 7:
-                result.append(.saturday)
-
-            default:
-                result = result + []
-            }
-        } // for weekday in self
-        return result
-    } // var EKWeekdays:  Array<EKWeekday>
-} // extension Array where Element == Int
+//extension Array where Element == Int {
+//    var EKWeekdays:  Array<EKWeekday> {
+//        var result: Array<EKWeekday> = []
+//        for weekday in self {
+//            assert(weekday >= 1)
+//            assert(weekday <= 7)
+//
+//            switch weekday {
+//            case 1:
+//                result.append(.sunday)
+//
+//            case 2:
+//                result.append(.monday)
+//
+//            case 3:
+//                result.append(.tuesday)
+//
+//            case 4:
+//                result.append(.wednesday)
+//
+//            case 5:
+//                result.append(.thursday)
+//
+//            case 6:
+//                result.append(.friday)
+//
+//            case 7:
+//                result.append(.saturday)
+//
+//            default:
+//                result = result + []
+//            }
+//        } // for weekday in self
+//        return result
+//    } // var EKWeekdays:  Array<EKWeekday>
+//} // extension Array where Element == Int
 
 
 // MARK:  -
