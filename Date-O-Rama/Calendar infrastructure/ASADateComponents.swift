49d92da400cdb06f123cb6201a48412247c343bd
Aaron Solomon Adelman <Aaron.Solomon.Adelman@gmail.com>
2021-02-11 23:32:34 +0200

    Disabled unused stuff.

--- Date-O-Rama/Calendar infrastructure/ASADateComponents.swift
+++ Date-O-Rama/Calendar infrastructure/ASADateComponents.swift
@@ -88,80 +88,80 @@ struct ASADateComponents:  Equatable {
     
     
     // MARK:  - Accessing Calendar Components
-    func value(for value: ASACalendarComponent) -> Int? {
-        //    Returns the value of one of the properties, using an enumeration value instead of a property name.
-        
-        switch value {
-        case .era:
-            return self.era
-        case .year:
-            return self.year
-        case .yearForWeekOfYear:
-            return self.yearForWeekOfYear
-        case .quarter:
-            return self.quarter
-        case .month:
-            return self.month
-        case .weekOfYear:
-            return self.weekOfYear
-        case .weekOfMonth:
-            return self.weekOfMonth
-        case .weekday:
-            return self.weekday
-        case .weekdayOrdinal:
-            return self.weekdayOrdinal
-        case .day:
-            return self.day
-        case .hour:
-            return self.hour
-        case .minute:
-            return self.minute
-        case .second:
-            return self.second
-        case .nanosecond:
-            return self.nanosecond
-        case .calendar, .timeZone:
-            debugPrint(#file, #function, "Some wiseguy seems to think a calendar or time zone is an integer.")
-            return nil // Cannot be cast to Int?
-        } // switch value
-    } // func value(for value: ASACalendarComponent) -> Int?
+//    func value(for value: ASACalendarComponent) -> Int? {
+//        //    Returns the value of one of the properties, using an enumeration value instead of a property name.
+//        
+//        switch value {
+//        case .era:
+//            return self.era
+//        case .year:
+//            return self.year
+//        case .yearForWeekOfYear:
+//            return self.yearForWeekOfYear
+//        case .quarter:
+//            return self.quarter
+//        case .month:
+//            return self.month
+//        case .weekOfYear:
+//            return self.weekOfYear
+//        case .weekOfMonth:
+//            return self.weekOfMonth
+//        case .weekday:
+//            return self.weekday
+//        case .weekdayOrdinal:
+//            return self.weekdayOrdinal
+//        case .day:
+//            return self.day
+//        case .hour:
+//            return self.hour
+//        case .minute:
+//            return self.minute
+//        case .second:
+//            return self.second
+//        case .nanosecond:
+//            return self.nanosecond
+//        case .calendar, .timeZone:
+//            debugPrint(#file, #function, "Some wiseguy seems to think a calendar or time zone is an integer.")
+//            return nil // Cannot be cast to Int?
+//        } // switch value
+//    } // func value(for value: ASACalendarComponent) -> Int?
     
-    mutating func setValue(_ value:  Int?, for component: ASACalendarComponent) {
-        //    Set the value of one of the properties, using an enumeration value instead of a property name.
-        
-        switch component {
-        case .era:
-            self.era = value
-        case .year:
-            self.year = value
-        case .yearForWeekOfYear:
-            self.yearForWeekOfYear = value
-        case .quarter:
-            self.quarter = value
-        case .month:
-            self.month = value
-        case .weekOfYear:
-            self.weekOfYear = value
-        case .weekOfMonth:
-            self.weekOfMonth = value
-        case .weekday:
-            self.weekday = value
-        case .weekdayOrdinal:
-            self.weekdayOrdinal = value
-        case .day:
-            self.day = value
-        case .hour:
-            self.hour = value
-        case .minute:
-            self.minute = value
-        case .second:
-            self.second = value
-        case .nanosecond:
-            self.nanosecond = value
-        case .calendar, .timeZone:
-            debugPrint(#file, #function, "Some wiseguy seems to think a calendar or time zone is an integer.")
-        } // switch component
-    } // mutating func setValue(_ value:  Int?, for component: ASACalendarComponent)
+//    mutating func setValue(_ value:  Int?, for component: ASACalendarComponent) {
+//        //    Set the value of one of the properties, using an enumeration value instead of a property name.
+//
+//        switch component {
+//        case .era:
+//            self.era = value
+//        case .year:
+//            self.year = value
+//        case .yearForWeekOfYear:
+//            self.yearForWeekOfYear = value
+//        case .quarter:
+//            self.quarter = value
+//        case .month:
+//            self.month = value
+//        case .weekOfYear:
+//            self.weekOfYear = value
+//        case .weekOfMonth:
+//            self.weekOfMonth = value
+//        case .weekday:
+//            self.weekday = value
+//        case .weekdayOrdinal:
+//            self.weekdayOrdinal = value
+//        case .day:
+//            self.day = value
+//        case .hour:
+//            self.hour = value
+//        case .minute:
+//            self.minute = value
+//        case .second:
+//            self.second = value
+//        case .nanosecond:
+//            self.nanosecond = value
+//        case .calendar, .timeZone:
+//            debugPrint(#file, #function, "Some wiseguy seems to think a calendar or time zone is an integer.")
+//        } // switch component
+//    } // mutating func setValue(_ value:  Int?, for component: ASACalendarComponent)
     
     
     // MARK: - Equatable
