116d7ef64d57b8cf96f2b956bcb92db3f545f194
Aaron Solomon Adelman <Aaron.Solomon.Adelman@gmail.com>
2021-06-01 11:03:02 +0300

    File reorganization.

---
+++ Date-O-Rama/Events/ASAEventSpecification.swift
@@ -0,0 +1,141 @@
+//
+//  ASAEventSpecification.swift
+//  Date-O-Rama
+//
+//  Created by אהרן שלמה אדלמן on 01/06/2021.
+//  Copyright © 2021 Adelsoft. All rights reserved.
+//
+
+import Foundation
+import EventKit
+
+class ASAEventSpecification: Codable {
+    var titles:  Dictionary<String, String>?
+    
+    var isAllDay:  Bool {
+        get {
+            return self.startDateSpecification.type == .allDay || self.startDateSpecification.type == .allMonth || self.startDateSpecification.type == .allYear
+        } // get
+    } // var isAllDay
+
+    var calendarCode:  ASACalendarCode?
+    var startDateSpecification:  ASADateSpecification
+    var endDateSpecification:  ASADateSpecification?
+    
+    var includeISOCountryCodes:  Array<String>?
+    var excludeISOCountryCodes:  Array<String>?
+        
+    var url: URL? // The URL for the calendar item.
+    var notes: String? // The notes associated with the calendar item.
+} // extension ASAEventSpecification
+
+
+// MARK:  -
+
+extension ASAEventSpecification {
+    func match(ISOCountryCode:  String?) -> Bool {
+        if ISOCountryCode == nil {
+            if self.includeISOCountryCodes != nil && self.includeISOCountryCodes != [] {
+                return false
+            }
+            
+            return true
+        } else {
+            if self.includeISOCountryCodes != nil {
+                let result = self.includeISOCountryCodes!.contains(ISOCountryCode!)
+                return result
+            }
+            
+            if self.excludeISOCountryCodes != nil {
+                let result = !self.excludeISOCountryCodes!.contains(ISOCountryCode!)
+                return result
+            }
+            
+            return true
+        }
+    } // func match(ISOCountryCode:  String?) -> Bool
+
+    func eventTitle(requestedLocaleIdentifier:  String, eventsFileDefaultLocaleIdentifier:  String) -> String? {
+        if self.titles != nil {
+            let titles = self.titles!
+
+            let userLocaleIdentifier = requestedLocaleIdentifier == "" ? Locale.autoupdatingCurrent.identifier : requestedLocaleIdentifier
+            let firstAttempt = titles[userLocaleIdentifier]
+            if firstAttempt != nil {
+                return firstAttempt
+            }
+
+            let userLanguageCode = userLocaleIdentifier.localeLanguageCode
+            if userLanguageCode != nil {
+                let secondAttempt = titles[userLanguageCode!]
+                if secondAttempt != nil {
+                    return secondAttempt
+                }
+            }
+
+            let thirdAttempt = titles[eventsFileDefaultLocaleIdentifier]
+            if thirdAttempt != nil {
+                return thirdAttempt
+            }
+
+            let fourthAttempt = titles["en"]
+            if fourthAttempt != nil {
+                return fourthAttempt
+            }
+        }
+
+        return nil
+    } // eventsFileDefaultLocaleIdentifier:  String) -> String?
+    
+    var recurrenceRules: [EKRecurrenceRule]? {
+        var result: [EKRecurrenceRule] = []
+                
+        if startDateSpecification.year == nil {
+            if startDateSpecification.yearDivisor != nil {
+                // Event repeats once every specific number of years
+                result.append(EKRecurrenceRule(recurrenceWith: .yearly, interval: startDateSpecification.yearDivisor!, end: nil))
+            } else {
+                // Event repeats every year
+                if startDateSpecification.month == nil {
+                    // Event repeats at least once every month
+                    
+                    if startDateSpecification.day == nil {
+                        if startDateSpecification.weekdays == nil {
+                            // Event repeats every day
+                            result.append(EKRecurrenceRule(recurrenceWith: .daily, interval: 1, end: nil))
+                        } else {
+                            // Event repeats every week
+                            result.append(EKRecurrenceRule(recurrenceWith: .weekly, interval: 1, end: nil))
+                        }
+
+                    } else {
+                        // Event repeats every month on a specific day
+                        result.append(EKRecurrenceRule(recurrenceWith: .monthly, interval: 1, end: nil))
+                    }
+                } else {
+                    // Event repeats every year
+                    result.append(EKRecurrenceRule(recurrenceWith: .yearly, interval: 1, end: nil))
+                }
+            }
+        }
+        
+        if result.count == 0 {
+            return nil
+        }
+        
+        return result
+    } // var recurrenceRules
+} // extension ASAEventSpecification
+
+
+// MARK: -
+
+enum ASAWeekday:  Int, Codable {
+    case sunday    = 1
+    case monday    = 2
+    case tuesday   = 3
+    case wednesday = 4
+    case thursday  = 5
+    case friday    = 6
+    case saturday  = 7
+} // enum ASAWeekday
--- Date-O-Rama/Events/ASAEventsFile.swift
+++ Date-O-Rama/Events/ASAEventsFile.swift
@@ -30,7 +30,7 @@ struct ASAEventsFile: Codable {
     var calendarColorAlpha:  CGFloat
     var calendarCode:  ASACalendarCode
     var otherCalendarCodes:  Array<ASACalendarCode>?
-    var eventSpecifications: Array<ASAInternalEventSpecification>
+    var eventSpecifications: Array<ASAEventSpecification>
     
     var calendarColor: UIColor {
         get {
@@ -49,137 +49,6 @@ struct ASAEventsFile: Codable {
         }
     }
 } // struct ASAEventsFile
-
-
-// MARK: - ASAInternalEventSpecification
-
-class ASAInternalEventSpecification: Codable {
-    var titles:  Dictionary<String, String>?
-    
-    var isAllDay:  Bool {
-        get {
-            return self.startDateSpecification.type == .allDay || self.startDateSpecification.type == .allMonth || self.startDateSpecification.type == .allYear
-        } // get
-    } // var isAllDay
-
-    var calendarCode:  ASACalendarCode?
-    var startDateSpecification:  ASADateSpecification
-    var endDateSpecification:  ASADateSpecification?
-    
-    var includeISOCountryCodes:  Array<String>?
-    var excludeISOCountryCodes:  Array<String>?
-        
-    var url: URL? // The URL for the calendar item.
-    var notes: String? // The notes associated with the calendar item.
-} // extension ASAInternalEventSpecification
-
-extension ASAInternalEventSpecification {
-    func match(ISOCountryCode:  String?) -> Bool {
-        if ISOCountryCode == nil {
-            if self.includeISOCountryCodes != nil && self.includeISOCountryCodes != [] {
-                return false
-            }
-            
-            return true
-        } else {
-            if self.includeISOCountryCodes != nil {
-                let result = self.includeISOCountryCodes!.contains(ISOCountryCode!)
-                return result
-            }
-            
-            if self.excludeISOCountryCodes != nil {
-                let result = !self.excludeISOCountryCodes!.contains(ISOCountryCode!)
-                return result
-            }
-            
-            return true
-        }
-    } // func match(ISOCountryCode:  String?) -> Bool
-
-    func eventTitle(requestedLocaleIdentifier:  String, eventsFileDefaultLocaleIdentifier:  String) -> String? {
-        if self.titles != nil {
-            let titles = self.titles!
-
-            let userLocaleIdentifier = requestedLocaleIdentifier == "" ? Locale.autoupdatingCurrent.identifier : requestedLocaleIdentifier
-            let firstAttempt = titles[userLocaleIdentifier]
-            if firstAttempt != nil {
-                return firstAttempt
-            }
-
-            let userLanguageCode = userLocaleIdentifier.localeLanguageCode
-            if userLanguageCode != nil {
-                let secondAttempt = titles[userLanguageCode!]
-                if secondAttempt != nil {
-                    return secondAttempt
-                }
-            }
-
-            let thirdAttempt = titles[eventsFileDefaultLocaleIdentifier]
-            if thirdAttempt != nil {
-                return thirdAttempt
-            }
-
-            let fourthAttempt = titles["en"]
-            if fourthAttempt != nil {
-                return fourthAttempt
-            }
-        }
-
-        return nil
-    } // eventsFileDefaultLocaleIdentifier:  String) -> String?
-    
-    var recurrenceRules: [EKRecurrenceRule]? {
-        var result: [EKRecurrenceRule] = []
-                
-        if startDateSpecification.year == nil {
-            if startDateSpecification.yearDivisor != nil {
-                // Event repeats once every specific number of years
-                result.append(EKRecurrenceRule(recurrenceWith: .yearly, interval: startDateSpecification.yearDivisor!, end: nil))
-            } else {
-                // Event repeats every year
-                if startDateSpecification.month == nil {
-                    // Event repeats at least once every month
-                    
-                    if startDateSpecification.day == nil {
-                        if startDateSpecification.weekdays == nil {
-                            // Event repeats every day
-                            result.append(EKRecurrenceRule(recurrenceWith: .daily, interval: 1, end: nil))
-                        } else {
-                            // Event repeats every week
-                            result.append(EKRecurrenceRule(recurrenceWith: .weekly, interval: 1, end: nil))
-                        }
-
-                    } else {
-                        // Event repeats every month on a specific day
-                        result.append(EKRecurrenceRule(recurrenceWith: .monthly, interval: 1, end: nil))
-                    }
-                } else {
-                    // Event repeats every year
-                    result.append(EKRecurrenceRule(recurrenceWith: .yearly, interval: 1, end: nil))
-                }
-            }
-        }
-        
-        if result.count == 0 {
-            return nil
-        }
-        
-        return result
-    } // var recurrenceRules
-} // extension ASAInternalEventSpecification
-
-
-// MARK: -
-
-enum ASAWeekday:  Int, Codable {
-    case sunday    = 1
-    case monday    = 2
-    case tuesday   = 3
-    case wednesday = 4
-    case thursday  = 5
-    case friday    = 6
-    case saturday  = 7
-} // enum ASAWeekday
 
 
 // MARK:  -
