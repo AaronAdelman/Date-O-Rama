4ba7a5a90a1a83b3f385ed5e4111859867425e81
Aaron Solomon Adelman <Aaron.Solomon.Adelman@gmail.com>
2021-09-19 20:16:18 +0300

    Cleanup.

--- Date-O-Rama/Views/Mini-clocks/ASAMiniClockView.swift
+++ Date-O-Rama/Views/Mini-clocks/ASAMiniClockView.swift
@@ -12,20 +12,21 @@ struct ASAMiniClockView:  View {
     var processedRow:  ASAProcessedRow
     var numberFormatter:  NumberFormatter
 
-    func progress() -> Double {
-        let secondsIntoDay:  Double = Double((processedRow.hour! * 60 + processedRow.minute!) * 60 + processedRow.second!)
-        let progress: Double = secondsIntoDay / Date.SECONDS_PER_DAY
-        
-        assert(progress >= 0.0)
-        assert(progress <= 1.0)
-        
-        return progress
-    } // func progress() -> Double
+//    func progress() -> Double {
+//        let secondsIntoDay:  Double = Double((processedRow.hour! * 60 + processedRow.minute!) * 60 + processedRow.second!)
+//        let progress: Double = secondsIntoDay / Date.SECONDS_PER_DAY
+//
+//        assert(progress >= 0.0)
+//        assert(progress <= 1.0)
+//
+//        return progress
+//    } // func progress() -> Double
 
     var body: some View {
         if processedRow.calendarType == .JulianDay {
 //             ASADayFractionView(progress: progress())
-            ASADayFractionView(progress: processedRow.fractionalHour!)
+            let progress: Double = processedRow.fractionalHour ?? 0.0
+            ASADayFractionView(progress: progress)
         } else {
             if processedRow.calendarCode.isSunsetTransitionCalendar {
                 let fractionalHours: Double = Double(processedRow.hour!) + Double(processedRow.minute!) / 60.0
@@ -33,7 +34,10 @@ struct ASAMiniClockView:  View {
                 let degrees = fractionalHours * degreesPerHour + 180.0
                 ASASolarTimeView(degrees: degrees, dimension: 56.0, font: .body)
             } else {
-                Watch(hour:  processedRow.hour!, minute:  processedRow.minute!, second:  processedRow.second!, isNight:  nightTime(hour:  processedRow.hour!, transitionType:  processedRow.transitionType), numberFormatter: numberFormatter)
+                let hour: Int = processedRow.hour!
+                let minute: Int = processedRow.minute!
+                let second: Int = processedRow.second!
+                Watch(hour:  hour, minute:  minute, second:  second, isNight:  nightTime(hour:  hour, transitionType:  processedRow.transitionType), numberFormatter: numberFormatter)
             }
         }
     } // var body
