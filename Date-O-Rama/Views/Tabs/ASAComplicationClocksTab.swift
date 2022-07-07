2671fb267c7b0ed8eb0f7623d9885539e4918643
Aaron Adelman <Aaron.Solomon.Adelman@gmail.com>
2022-07-07 12:29:53 +0300

    It is now possible to edit the locations of complications.

--- Date-O-Rama/Views/Tabs/ASAComplicationClocksTab.swift
+++ Date-O-Rama/Views/Tabs/ASAComplicationClocksTab.swift
@@ -14,58 +14,34 @@ struct ASAComplicationClocksTab: View {
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     @State var isNavigationBarHidden:  Bool = true
     
-    func clockArray(with key: ASAClockArrayKey) -> ASALocationWithClocks {
-        switch key {
-        case .threeLineLarge:
-            return self.userData.threeLineLargeClocks
-        case .twoLineLarge:
-            return self.userData.twoLineLargeClocks
-        case .app:
-            return self.userData.mainClocks[0]
-        case .twoLineSmall:
-            return self.userData.twoLineSmallClocks
-        case .oneLineLarge:
-            return self.userData.oneLineLargeClocks
-        case .oneLineSmall:
-            return self.userData.oneLineSmallClocks
-        } // switch key
-    } // func clockArray(with key: ASAClockArrayKey) -> ASALocationWithClocks
+    //    func clockArray(with key: ASAClockArrayKey) -> ASALocationWithClocks {
+    //        switch key {
+    //        case .threeLineLarge:
+    //            return self.userData.threeLineLargeClocks
+    //        case .twoLineLarge:
+    //            return self.userData.twoLineLargeClocks
+    //        case .app:
+    //            return self.userData.mainClocks[0]
+    //        case .twoLineSmall:
+    //            return self.userData.twoLineSmallClocks
+    //        case .oneLineLarge:
+    //            return self.userData.oneLineLargeClocks
+    //        case .oneLineSmall:
+    //            return self.userData.oneLineSmallClocks
+    //        } // switch key
+    //    } // func clockArray(with key: ASAClockArrayKey) -> ASALocationWithClocks
     
     var body: some View {
         NavigationView {
             VStack {
                 Rectangle().frame(height:  0.0) // Prevents content from showing through the status bar.
                 List {
-                    ForEach(ASAClockArrayKey.complicationSections, id:  \.self) {complicationKey in
-                        let locationWithClocks = self.clockArray(with: complicationKey)
-                        let location = locationWithClocks.location
-                        
-                        let sectionHeaderEmoji = (location.regionCode ?? "").flag
-                        let sectionHeaderTitle = location.formattedOneLineAddress
-                        let sectionHeaderFont: Font = Font.title2
-                        
-                        Section(header: VStack {
-                            Text(NSLocalizedString(complicationKey.rawValue, comment: ""))
-                                .lineLimit(2)
-                            HStack {
-                                if locationWithClocks.usesDeviceLocation {
-                                    ASALocationSymbol()
-                                }
-                                Text(sectionHeaderEmoji)
-                                Text(sectionHeaderTitle)
-                                    .lineLimit(2)
-                            } // HStack
-                        } // VStack
-                            .font(sectionHeaderFont)
-                                , content: {
-                            ForEach(locationWithClocks.clocks, id:  \.uuid) {
-                                clock
-                                in
-                                ASAClockCell(processedClock: ASAProcessedClock(clock: clock, now: now, isForComplications: true), now: $now, shouldShowTime: false, shouldShowMiniCalendar: false, isForComplications: true, indexIsOdd: false)
-                            }
-                        })
+                    ForEach(ASAClockArrayKey.complicationSections, id:  \.self) {
+                        complicationKey
+                        in
+                        ASAComplicationSectionView(complicationKey: complicationKey, now: $now)
                     }
-                }
+                } // List
                 .listStyle(InsetGroupedListStyle())
                 
                 .colorScheme(.dark)
@@ -83,6 +59,84 @@ struct ASAComplicationClocksTab: View {
         }
     }
 } // struct ASAComplicationClocksTab
+
+
+struct ASAComplicationSectionView: View {
+    var complicationKey: ASAClockArrayKey
+    @Binding var now: Date
+    
+    let userData = ASAUserData.shared
+    
+    func clockArray(with key: ASAClockArrayKey) -> ASALocationWithClocks {
+        switch key {
+        case .threeLineLarge:
+            return self.userData.threeLineLargeClocks
+        case .twoLineLarge:
+            return self.userData.twoLineLargeClocks
+        case .app:
+            return self.userData.mainClocks[0]
+        case .twoLineSmall:
+            return self.userData.twoLineSmallClocks
+        case .oneLineLarge:
+            return self.userData.oneLineLargeClocks
+        case .oneLineSmall:
+            return self.userData.oneLineSmallClocks
+        } // switch key
+    } // func clockArray(with key: ASAClockArrayKey) -> ASALocationWithClocks
+    
+    @State private var showingDetailView = false
+    
+    var body: some View {
+        let locationWithClocks = self.clockArray(with: complicationKey)
+        let location = locationWithClocks.location
+        
+        let sectionHeaderEmoji = (location.regionCode ?? "").flag
+        let sectionHeaderTitle = location.formattedOneLineAddress
+        let sectionHeaderFont: Font = Font.title2
+        
+        Section(header: VStack {
+            Text(NSLocalizedString(complicationKey.rawValue, comment: ""))
+                .lineLimit(2)
+            HStack {
+                if locationWithClocks.usesDeviceLocation {
+                    ASALocationSymbol()
+                }
+                Text(sectionHeaderEmoji)
+                Text(sectionHeaderTitle)
+                    .lineLimit(2)
+                
+                Spacer()
+                Menu {
+                    Button(
+                        action: {
+                            self.showingDetailView = true
+                            
+                        }
+                    ) {
+                        HStack {
+                            Image(systemName: "pencil")
+                            Text("Edit location")
+                        } // HStack
+                    }
+                } label: {
+                    Image(systemName: "arrow.down.square.fill")
+                }
+                .sheet(isPresented: self.$showingDetailView, onDismiss: {
+                }) {
+                    ASALocationChooserView(locationWithClocks: locationWithClocks, shouldCreateNewLocationWithClocks: false)
+                }
+            } // HStack
+        } // VStack
+            .font(sectionHeaderFont)
+                , content: {
+            ForEach(locationWithClocks.clocks, id:  \.uuid) {
+                clock
+                in
+                ASAClockCell(processedClock: ASAProcessedClock(clock: clock, now: now, isForComplications: true), now: $now, shouldShowTime: false, shouldShowMiniCalendar: false, isForComplications: true, indexIsOdd: false)
+            }
+        })
+    }
+}
 
 struct ASAComplicationClocksView_Previews: PreviewProvider {
     static var previews: some View {
