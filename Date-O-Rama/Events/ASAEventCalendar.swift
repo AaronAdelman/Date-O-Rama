f16886ad90577b436b4acaa5b25d2b3def733627
Aaron Adelman <Aaron.Solomon.Adelman@gmail.com>
2023-01-19 22:33:14 +0200

    Crash quick fix.

--- Date-O-Rama/Events/ASAEventCalendar.swift
+++ Date-O-Rama/Events/ASAEventCalendar.swift
@@ -1125,7 +1125,8 @@ class ASAEventCalendar {
             assert(previousSunset.oneDayAfter > date)
             
             assert(startOfDay <= date)
-            assert(date < startOfNextDay)
+//            assert(date < startOfNextDay)
+            // TODO:  Assertion failure here!
             let newEvent = processEventSpecification(calendar: calendar, eventSpecification: eventSpecification, otherCalendars: otherCalendars, components: components, date: date, locationData: locationData, startOfDay: startOfDay, startOfNextDay: startOfNextDay, previousSunset: previousSunset, nightHourLength: nightHourLength, sunrise: sunrise, hourLength: hourLength, previousOtherDusk: previousOtherDusk, otherNightHourLength: otherNightHourLength, otherDawn: otherDawn, otherHourLength: otherHourLength, regionCode: regionCode, location: location, timeZone: timeZone, requestedLocaleIdentifier: requestedLocaleIdentifier, eventCalendarName: eventCalendarName, calendarTitleWithoutLocation: calendarTitleWithoutLocation, clock: clock)
             
             if newEvent != nil {
