a2eb039a8b49c40563497ba1d15270a24425179e
Aaron Adelman <Aaron.Solomon.Adelman@gmail.com>
2025-12-11 09:28:32 +0200

    Preparation.

--- Date-O-Rama/Calendars/ASASolarTimeCalendar.swift
+++ Date-O-Rama/Calendars/ASASolarTimeCalendar.swift
@@ -245,6 +245,7 @@ public class ASASolarTimeCalendar:  ASACalendar, ASACalendarWithWeeks, ASACalend
         return result
     } // func hoursMinutesSecondsTimeString(hours: Double, symbol: String, localeIdentifier: String, minutesPerHour: Double, secondsPerMinutes: Double, minimumHourDigits: Int, minimumMinuteDigits: Int, minimumSecondDigits: Int) -> String
     
+    // TODO: Point of expansion
     func dateString(fixedNow: Date, localeIdentifier: String, timeZone: TimeZone, dateFormat: ASADateFormat) -> String {
         self.dateFormatter.apply(localeIdentifier: localeIdentifier, timeFormat: .none, timeZone: timeZone)
         
