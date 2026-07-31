e9de00c871c53bc559af720a7f7678fc9efa579b
Aaron Adelman <Aaron.Solomon.Adelman@gmail.com>
2026-07-31 09:39:59 +0300

    Key polishing.

--- Date-O-Rama/Extensions/EKEventExtensions.swift
+++ Date-O-Rama/Extensions/EKEventExtensions.swift
@@ -71,8 +71,7 @@ extension EKEvent:  ASASingleEvent {
         if self.birthdayContactIdentifier != nil {
             let templatesFile = ASAEventSpecification.templateEventsFile
             if templatesFile != nil {
-//                let birthdayTemplate = templatesFile!.eventSpecifications.first(where: {$0.template == "*BDay*"})
-                let birthdayTemplate = templatesFile!.templateSpecifications?["*BDay*"]
+                let birthdayTemplate = templatesFile!.templateSpecifications?["*BD*"]
                 if birthdayTemplate != nil {
                     return birthdayTemplate!.emoji
                 }
